43c828792bf381e406af3dd8429a0dc1c97ddadc
Stepan <petrivs572@gmail.com>
2024-03-12 19:27:02 +0200

    Solved "Greed is good" task

---
+++ src/main/java/org/example/GreedIsGood/Greed.java
@@ -0,0 +1,26 @@
+package org.example.GreedIsGood;
+
+import java.util.Arrays;
+
+public class Greed {
+    public static int greedy(int[] dice){
+        //code here
+        int[] counts = new int[7];
+
+        for (int value : dice) {
+            counts[value]++;
+        }
+
+        int score = 0;
+
+        for (int i = 1; i < counts.length; i++) {
+            int triplet = i == 1 ? 1000 : i * 100;
+            score += counts[i] / 3 * triplet;
+        }
+
+        score += counts[1] % 3 * 100;
+        score += counts[5] % 3 * 50;
+
+        return score;
+    }
+}
